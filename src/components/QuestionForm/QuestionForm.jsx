297eddbf3616595a33153c58efeee497d04b7c19
Alexandra <aleksandrakurilo61@gmail.com>
2026-02-10 22:42:38 +0200

    edit card action, date format helper

--- src/components/QuestionForm/QuestionForm.jsx
+++ src/components/QuestionForm/QuestionForm.jsx
@@ -6,6 +6,7 @@ export const QuestionForm = ({ formAction, state, isPending, submitBtnText }) =>
 
   return (
     <form action={formAction} className={cls.form}>
+      <input type="text" name="questionId" defaultValue={state.id} hidden />
       <div className={cls.formControl}>
         <label htmlFor="questionField">Question: </label>
         <textarea
